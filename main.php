018543c9b790bb9117e9d62ca2dd4d9c8313ef8e
Deepu Mohan Puthrote <mail@deepumohan.com>
2011-12-05 00:32:41 +0000

    Updated tech blog link

--- main.php
+++ main.php
@@ -10,7 +10,7 @@
                     <td><a href="https://plus.google.com/106447761403337564868" title="deepu" target="_blank"><div class="social-buzz imagedropshadow"></div></a></td>
                     <td><a href="http://in.linkedin.com/in/deepumohanp" title="deepu" target="_blank"><div class="social-linkedin imagedropshadow"></div></a></td>
                     <td><a href="http://blog.deepumohan.com" title="deepu" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
-                    <td><a href="http://tips.deepumohan.com" title="deepu" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
+                    <td><a href="http://tech.deepumohan.com" title="deepu" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
                 </tr>
             </table>
         </div>
