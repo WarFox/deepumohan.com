e5722333d4ca0967dd9f1425718ec94e4b4feb00
Deepu Mohan Puthrote <mail@deepumohan.com>
2011-12-05 00:24:31 +0000

    Removed footer links and removed height attribute from style file for footer

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
     
     <div class="clear"></div>
     <footer id="footer">
-        <ul>
+        <!--ul>
             <li><a href="http://www.facebook.com/deepu.mohan.p" title="deepu" target="_blank" >Facebook</a></li> |
             <li><a href="http://blog.deepumohan.com" title="deepu" target="_blank">Blog</a></li> |
             <li><a href="http://www.twitter.com/deepumohanp" title="deepu" target="_blank">Twitter</a></li> |
             <li><a href="https://profiles.google.com/u/0/106447761403337564868/buzz" title="deepu" target="_blank">Buzz</a></li> |
             <li><a href="http://in.linkedin.com/in/deepumohanp" title="deepu" target="_blank">LinkedIn</a></li>            
-        </ul>
+        </ul-->
         <hr/>
         <div class="clear"></div>
         <div class="lt">Copyright &copy; <?php print date("Y"); ?> WarFox Inc. <?php print Date("F d, Y"); ?> All rights reserved</div>
