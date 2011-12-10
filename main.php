8b82f57623d5f71b2f7d13a4c1ca3f04c4c0a4b3
Deepu Mohan Puthrote <mail@deepumohan.com>
2011-12-10 17:16:08 +0000

    Added google web font Cagliostro in style.css and added github in main.php

--- main.php
+++ main.php
@@ -1,16 +1,28 @@
     <div class="clear"></div>
+      <div id="fb-root"></div>
+      <script>
+        (function(d, s, id) {
+          var js, fjs = d.getElementsByTagName(s)[0];
+          if (d.getElementById(id)) {return;}
+          js = d.createElement(s); js.id = id;
+          js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+          fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+      </script>
+      <div class="fb-like"></div>
     <div id="main">
         <div class="clear"></div>
         <div id="social">
-            <h2>Deepu Mohan Puthorte</h2>
+            <h1>deepu mohan puthorte</h1>
             <table cellpadding="0" cellspacing="0" id="social_table">
                 <tr>
-                    <td><a href="http://www.facebook.com/deepu.mohan.p" title="deepu" target="_blank" ><div class="social-facebook imagedropshadow"></div></a></td>
-                    <td><a href="http://www.twitter.com/deepumohanp" title="deepu" target="_blank"><div class="social-twitter imagedropshadow"></div></a></td>
-                    <td><a href="https://plus.google.com/106447761403337564868" title="deepu" target="_blank"><div class="social-buzz imagedropshadow"></div></a></td>
-                    <td><a href="http://in.linkedin.com/in/deepumohanp" title="deepu" target="_blank"><div class="social-linkedin imagedropshadow"></div></a></td>
-                    <td><a href="http://blog.deepumohan.com" title="deepu" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
-                    <td><a href="http://tech.deepumohan.com" title="deepu" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
+                    <td><a href="http://www.facebook.com/deepu.mohan.p" title="Deepu @ Facebook" target="_blank" ><div class="social-facebook imagedropshadow"></div></a></td>
+                    <td><a href="http://www.twitter.com/deepumohanp" title="Deepu @ Twitter" target="_blank"><div class="social-twitter imagedropshadow"></div></a></td>
+                    <td><a href="https://plus.google.com/106447761403337564868" title="Deepu @ Google+" target="_blank"><div class="social-buzz imagedropshadow"></div></a></td>
+                    <td><a href="http://in.linkedin.com/in/deepumohanp" title="Deepu @ LinkedIn" target="_blank"><div class="social-linkedin imagedropshadow"></div></a></td>
+                    <td><a href="http://blog.deepumohan.com" title="My Blog" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
+                    <td><a href="http://tech.deepumohan.com" title="My Techno Blog" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
+                    <td><a href="https://github.com/WarFox" title="I'm on github" target="_blank"><div class="social-github imagedropshadow"></div></a></td>
                 </tr>
             </table>
         </div>
