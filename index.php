0426b1041aaa3973195762037f5911ad3546a91c
Deepu Mohan Puthrote <mail@deepumohan.com>
2011-12-10 17:23:53 +0000

    Modified meta content in index.php

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
-﻿<!DOCTYPE HTML>
+﻿<!DOCTYPE html>
 <html>
     <head>
-        <meta name="description" content="Personal Website of Deepu Mohan Puthrote, a Technology enthusiast" />
-        <meta name="keywords" content="Deepu,Mohan,Puthrote,Deepu Mohan, Deepu Puthrote, Mohan Puthrote, Deepu Mohan P" />
-        <meta name="author" content="Deepu Mohan P" />
         <meta charset="UTF-8">
+        <meta name="description" content="Personal Website of Deepu Mohan Puthrote, a Technology enthusiast" />
+        <meta name="keywords" content="Deepu,Mohan,Puthrote,Deepu Mohan, Deepu Puthrote, Mohan Puthrote, Deepu Mohan Puthrote" />
+        <meta name="author" content="Deepu Mohan Puthrote" />
+	<meta name="classification" content="public" />
+	<meta name="resource-type" content="document" />
+	<meta name="copyright" content="Deepu Mohan Puthrote" />
+
         <meta http-equiv="X-UA-Compatible" content="chrome=1">
-        <title>Personal Website of Deepu Mohan P | A Technology enthusiast</title>
-        <!--link href='http://fonts.googleapis.com/css?family=Dancing+Script' rel='stylesheet' type='text/css'-->
+
+        <title>Personal Website of Deepu Mohan Puthrote | A Technology enthusiast</title>
+
         <style type="text/css">@import url('files/css/style.css');</style>
-        <script type="text/javascript" src="files/js/jquery.js"></script>
-        <script type="text/javascript" src="files/js/jquery.center.js"></script>
+
+        <script type="text/javascript" charset="utf-8" src="files/js/jquery.js"></script>
+        <script type="text/javascript" charset="utf-8" src="files/js/jquery.center.js"></script>
+
         <script type="text/javascript">
             $(document).ready(function () {
                 $('.post').center({direction:'horiz'});
@@ -25,3 +32,4 @@
         <?php include ("footer.php"); ?>        
     </body>
 </html>
+
