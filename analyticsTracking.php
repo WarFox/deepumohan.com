be56a08349424f738f2e4fedae76c7dc4d149e9f
Deepu Mohan Puthrote <mail@deepumohan.com>
2012-01-28 17:50:04 +0000

    removed commented analytics tracking code

--- analyticsTracking.php
+++ analyticsTracking.php
@@ -1,18 +1,4 @@
-        <!--Start Analytics tracking code -->
-        <!--script type="text/javascript">
-
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-1146230-5']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-
-        </script-->
-        <!--new subdomain tracking code-->
+    <!--new subdomain tracking code-->
 	<script type="text/javascript">
 	
 	  var _gaq = _gaq || [];
@@ -28,4 +14,5 @@
 	  })();
 	
 	</script>        
-        <!-- End Analytics tracking code -->
+    <!-- End Analytics tracking code -->
+
