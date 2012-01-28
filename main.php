8f7e528eff3d8cd6cdaf5cf8a7b7ca65063d739a
Deepu Mohan Puthrote <mail@deepumohan.com>
2012-01-28 14:35:39 +0000

    Converted table layout to ul and li layout using css

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
     <div class="clear"></div>
-      <div id="fb-root"></div>
-      <script>
+    <div id="fb-root"></div>
+    <script>
         (function(d, s, id) {
           var js, fjs = d.getElementsByTagName(s)[0];
           if (d.getElementById(id)) {return;}
@@ -8,22 +8,20 @@
           js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
           fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
-      </script>
-      <div class="fb-like"></div>
+    </script>
+    <div class="fb-like"></div>
     <div id="main">
         <div class="clear"></div>
         <div id="social">
             <h1>deepu mohan puthorte</h1>
-            <table cellpadding="0" cellspacing="0" id="social_table">
-                <tr>
-                    <td><a href="http://www.facebook.com/deepu.mohan.p" title="Deepu @ Facebook" target="_blank" ><div class="social-facebook imagedropshadow"></div></a></td>
-                    <td><a href="http://www.twitter.com/deepumohanp" title="Deepu @ Twitter" target="_blank"><div class="social-twitter imagedropshadow"></div></a></td>
-                    <td><a href="https://plus.google.com/106447761403337564868" title="Deepu @ Google+" target="_blank"><div class="social-buzz imagedropshadow"></div></a></td>
-                    <td><a href="http://in.linkedin.com/in/deepumohanp" title="Deepu @ LinkedIn" target="_blank"><div class="social-linkedin imagedropshadow"></div></a></td>
-                    <td><a href="http://blog.deepumohan.com" title="My Blog" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
-                    <td><a href="http://tech.deepumohan.com" title="My Techno Blog" target="_blank"><div class="social-blogger imagedropshadow"></div></a></td>
-                    <td><a href="https://github.com/WarFox" title="I'm on github" target="_blank"><div class="social-github imagedropshadow"></div></a></td>
-                </tr>
-            </table>
+            <ul class="social">
+                    <li><a href="http://www.facebook.com/deepu.mohan.p" title="Deepu @ Facebook" ><img src="files/css/images/transparent.gif" class="social-facebook imagedropshadow" alt="Deepu @ Facebook"/></a></li>
+                    <li><a href="http://www.twitter.com/deepumohanp" title="Deepu @ Twitter" ><img src="files/css/images/transparent.gif" class="social-twitter imagedropshadow" alt="Deepu @ Twitter"/></a></li>
+                    <li><a href="https://plus.google.com/106447761403337564868" title="Deepu @ Google+"><img src="files/css/images/transparent.gif" class="social-buzz imagedropshadow" alt="Deepu @ Google+"/></a></li>
+                    <li><a href="http://in.linkedin.com/in/deepumohanp" title="Deepu @ LinkedIn"><img src="files/css/images/transparent.gif" class="social-linkedin imagedropshadow" alt="Deepu @ LinkedIn"/></a></li>
+                    <li><a href="http://blog.deepumohan.com" title="My Blog"><img src="files/css/images/transparent.gif" class="social-blogger imagedropshadow" alt="My Blog"/></a></li>
+                    <li><a href="http://tech.deepumohan.com" title="My Technology Blog"><img src="files/css/images/transparent.gif" class="social-blogger imagedropshadow" alt="My Technology Blog"/></a></li>
+                    <li><a href="https://github.com/WarFox" title="I'm on github"><img src="files/css/images/transparent.gif" class="social-github imagedropshadow" alt="Deepu's github page"/></a></li>
+            </ul>
         </div>
-    </div>
+    </div>    
