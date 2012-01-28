4967f66c4b714f41da5001ad86941be768959804
Deepu Mohan Puthrote <mail@deepumohan.com>
2012-01-28 16:13:23 +0000

    Removed facebook like button

--- main.php
+++ main.php
@@ -1,22 +1,10 @@
-    <div class="clear"></div>
-    <div id="fb-root"></div>
-    <script>
-        (function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) {return;}
-          js = d.createElement(s); js.id = id;
-          js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-    <div class="fb-like"></div>
     <div id="main">
         <div class="clear"></div>
         <div id="social">
             <h1>deepu mohan puthrote</h1>
             <ul class="social">
-                    <li><a href="http://www.facebook.com/deepu.mohan.p" title="Deepu @ Facebook" ><img src="files/css/images/transparent.gif" class="social-facebook imagedropshadow" alt="Deepu @ Facebook"/></a></li>
-                    <li><a href="http://www.twitter.com/deepumohanp" title="Deepu @ Twitter" ><img src="files/css/images/transparent.gif" class="social-twitter imagedropshadow" alt="Deepu @ Twitter"/></a></li>
+                    <li><a href="https://www.facebook.com/deepu.mohan.p" title="Deepu @ Facebook" ><img src="files/css/images/transparent.gif" class="social-facebook imagedropshadow" alt="Deepu @ Facebook"/></a></li>
+                    <li><a href="https://www.twitter.com/deepumohanp" title="Deepu @ Twitter" ><img src="files/css/images/transparent.gif" class="social-twitter imagedropshadow" alt="Deepu @ Twitter"/></a></li>
                     <li><a href="https://plus.google.com/106447761403337564868" title="Deepu @ Google+"><img src="files/css/images/transparent.gif" class="social-buzz imagedropshadow" alt="Deepu @ Google+"/></a></li>
                     <li><a href="http://in.linkedin.com/in/deepumohanp" title="Deepu @ LinkedIn"><img src="files/css/images/transparent.gif" class="social-linkedin imagedropshadow" alt="Deepu @ LinkedIn"/></a></li>
                     <li><a href="http://blog.deepumohan.com" title="My Blog"><img src="files/css/images/transparent.gif" class="social-blogger imagedropshadow" alt="My Blog"/></a></li>
