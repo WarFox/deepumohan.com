1df0b03561062e4626e7dbbb3cff98f00c45380e
Deepu Mohan Puthrote <mail@deepumohan.com>
2012-01-28 18:37:18 +0000

    Updated href in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
     
     <footer id="footer">
-	<a href="">
+	<a href="https://github.com/WarFox/deepumohan.com">
 	<img class="github-32x32" src="files/css/images/transparent.gif" height=32px width=32px />
-	This site is open source
+	This site is open source.
 	</a>
     </footer>
