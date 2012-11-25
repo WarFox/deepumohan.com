77f582688e9bfc8d974f1b5bb150402a63cbd786
Deepu Mohan Puthrote <github@deepumohan.com>
2012-11-25 14:55:14 +0000

    Remove unwanted/unused files

--- copyright.php
+++
@@ -1,5 +0,0 @@
-
-    <div class="footer copyright">
-        <hr/>
-        Copyright &copy;<?php print date("Y"); ?> WarFox Inc. <?php print Date("F d, Y"); ?>
-    </div>
--- footer.php
+++
@@ -1,4 +0,0 @@
-
-    <footer id="footer">
-	    <a href="https://github.com/WarFox/deepumohan.com" class="github-32x32">This site is open source.</a>
-    </footer>
