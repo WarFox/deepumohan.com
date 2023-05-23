ca4763ab750faa47d883fd674e07421d5596cc31
Deepu Mohan Puthrote <git@deepumohan.com>
2023-05-23 22:13:33 +0100

    Add DocumentDrivenNotFound page and remove 404 page

---
+++ components/DocumentDrivenNotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div data-content-id="content:404.md">
+    <h1 id="page-not-found-️">404 Page Not Found 🤷‍♂️</h1>
+    <p>
+      <nuxt-link to="https://http.dev/404">https://http.dev/404</nuxt-link>
+    </p>
+    <p>
+      <nuxt-link to="https://httpstatuses.io/404"
+        >https://httpstatuses.io/404</nuxt-link
+      >
+    </p>
+    <p>Sorry, but the page you were trying to view does not exist</p>
+    <p>
+      It looks like this was the result of either, a mistyped address or an
+      out-of-date link.
+    </p>
+    <p>Please check the URL in the address bar and try again.</p>
+    <p>Go to <nuxt-link to="/">homepage</nuxt-link>.</p>
+  </div>
+</template>
